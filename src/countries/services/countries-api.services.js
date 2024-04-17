065607bc9d92c8c9165711bd510ca252ceac0ed4
IanHD04 <u202214059@upc.edu.pe>
2024-04-16 23:46:44 -0500

    Feat: Add api service

--- src/countries/services/countries-api.services.js
+++ src/countries/services/countries-api.services.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
+//URL que nos den, de donde sacaremos la data
 const http= axios.create({
     baseURL: 'https://restcountries.com/v3.1/name/'
 })
 
+//Ruta del pais de donde se sacara la info
 export class countriesApiService {
     getCountry(){
-        return http().get('peru');
+        return http.get('peru');
     }
 }
