c97e50b6be21e571834053fe4d7f2801955e8d2f
IanHD04 <u202214059@upc.edu.pe>
2024-04-17 00:00:06 -0500

    Feat: Add components en main.js

--- src/main.js
+++ src/main.js
@@ -1,22 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Primeiro from 'primevue/config';
+import PrimeVue from 'primevue/config';
 
+
+//Impotaciones generales
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
-
 import 'primeflex/primeflex.css';
 
+
+//Los componentes que se utilizaran
 import Card from 'primevue/card'
 import Toolbar from 'primevue/toolbar';
 import Menubar from 'primevue/menubar';
 import Menu from 'primevue/menu'
-import PrimeVue from "primevue/config";
 
 
+//Se inicializan los componentes
 createApp(App)
     .use(PrimeVue,{ripple:true})
     .component('pv-card',Card)
     .component('pv-toolbar',Toolbar)
     .component('pv-menubar',Menubar)
     .component('pv-menu',Menu)
+    .mount('#app')
